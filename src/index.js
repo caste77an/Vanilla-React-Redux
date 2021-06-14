0074e8a2c9e6710a902018ab685cec4ea5ebc314
castle <castle@castleui-iMac.local>
2021-06-14 17:03:27 +0900

    #1.3 Subscriptions

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import { createStore } from "redux";
 
-// const add = document.getElementById("add");
-// const minus = document.getElementById("minus");
-// const number = document.querySelector("span");
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+number.innerText = 0;
 
 const countModifier = (count = 0, action) => {
-  console.log(count, action);
   if (action.type === "ADD") {
     return count + 1;
   } else if (action.type === "MINUS") {
@@ -14,14 +15,14 @@ const countModifier = (count = 0, action) => {
     return count;
   }
 };
-
 const countStore = createStore(countModifier);
 
-countStore.dispatch({type: "ADD"});
-countStore.dispatch({type: "ADD"});
-countStore.dispatch({type: "ADD"});
-countStore.dispatch({type: "ADD"});
-countStore.dispatch({type: "ADD"});
-countStore.dispatch({type: "MINUS"});
+const onChange = () => {
+  number.innerText = countStore.getState();
+}
+// subscribe를 이용하여 countStore에 변화가 있을때 마다 onChange를 실행함
+countStore.subscribe(onChange);
 
-console.log(countStore.getState());
+// add에 event Listener를 추가하여 dispatch를 진행함
+add.addEventListener("click", () => countStore.dispatch({ type: "ADD" }));
+minus.addEventListener("click", () => countStore.dispatch({ type: "MINUS" }));
