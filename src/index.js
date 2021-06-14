34857816ef9c114a0aeb8c42418f12cf171c0941
castle <castle@castleui-iMac.local>
2021-06-14 16:05:27 +0900

    #1.2 Actions

--- src/index.js
+++ src/index.js
@@ -4,9 +4,24 @@ import { createStore } from "redux";
 // const minus = document.getElementById("minus");
 // const number = document.querySelector("span");
 
-const reducer = (state) => {
-	// modify state function is here ! 
-	return state;
+const countModifier = (count = 0, action) => {
+  console.log(count, action);
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
 
-const countStore = createStore(reducer);
+const countStore = createStore(countModifier);
+
+countStore.dispatch({type: "ADD"});
+countStore.dispatch({type: "ADD"});
+countStore.dispatch({type: "ADD"});
+countStore.dispatch({type: "ADD"});
+countStore.dispatch({type: "ADD"});
+countStore.dispatch({type: "MINUS"});
+
+console.log(countStore.getState());
