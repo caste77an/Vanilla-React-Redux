bb1469881167abb6cbf13bf3bd526e45a13cf722
castle <castle@castleui-iMac.local>
2021-06-14 17:22:45 +0900

    #1.4 Recap Refactor

--- src/index.js
+++ src/index.js
@@ -6,13 +6,19 @@ const number = document.querySelector("span");
 
 number.innerText = 0;
 
+// 변수에 문자열을 담는 형식으로 써야 에러 났을때 발견 가능함
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 const countModifier = (count = 0, action) => {
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
-  } else {
-    return count;
+  // if else 를 쓰는것 보다 switch case가 가독성 좋음
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 };
 const countStore = createStore(countModifier);
@@ -24,5 +30,5 @@ const onChange = () => {
 countStore.subscribe(onChange);
 
 // add에 event Listener를 추가하여 dispatch를 진행함
-add.addEventListener("click", () => countStore.dispatch({ type: "ADD" }));
-minus.addEventListener("click", () => countStore.dispatch({ type: "MINUS" }));
+add.addEventListener("click", () => countStore.dispatch({ type: ADD }));
+minus.addEventListener("click", () => countStore.dispatch({ type: MINUS }));
